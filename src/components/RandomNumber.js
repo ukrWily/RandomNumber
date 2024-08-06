6a6ee325873c0d0f8075727a2f0c0477c2a076a9
ukrWily <ukrwily@gmail.com>
2024-08-06 13:45:56 +0300

    L added generateRandomNums

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,15 +1,13 @@
 import { useState } from "react";
-
-function generateRandomNum() {
-  return Math.floor(Math.random() * 100);
-}
-
+import generateRandomNum from "../utils/generateRandomNums";
+//
 function RandomNumber() {
   const [randomNum, setRandomNum] = useState(generateRandomNum());
+  //======================================
   const changeRandomNum = () => {
     setRandomNum(generateRandomNum());
   };
-
+  //======================================
   return (
     <div>
       <h1>{randomNum}</h1>
---
+++ src/utils/generateRandomNums.js
@@ -0,0 +1,5 @@
+function generateRandomNum() {
+  return Math.floor(Math.random() * 1000);
+}
+
+export default generateRandomNum;
